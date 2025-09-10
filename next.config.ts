a84577ade4ef12541b3e055158160a75fc93c2c9
Kunal Rawat <kr0324527@gmail.com>
2025-09-10 15:25:59 +0000

    if small image is problem you can use flag cdn instead <img   src="htt

--- next.config.ts
+++ next.config.ts
@@ -24,7 +24,7 @@ const nextConfig: NextConfig = {
       },
       {
         protocol: 'https',
-        hostname: 'flagsapi.com',
+        hostname: 'flagcdn.com',
         port: '',
         pathname: '/**',
       }
